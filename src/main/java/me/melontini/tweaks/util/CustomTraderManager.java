2386f337909f1e921a750b30d4eb6e74e2a8dba8
melontini <melontini@yandex.ru>
2022-07-18 16:36:52 +0700

    Just wrote the worst piece of code ever How's your day going?

---
+++ src/main/java/me/melontini/tweaks/util/CustomTraderManager.java
@@ -0,0 +1,110 @@
+package me.melontini.tweaks.util;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.SpawnRestriction;
+import net.minecraft.entity.passive.TraderLlamaEntity;
+import net.minecraft.entity.passive.WanderingTraderEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.tag.BiomeTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.SpawnHelper;
+import net.minecraft.world.WorldView;
+import net.minecraft.world.level.ServerWorldProperties;
+import net.minecraft.world.poi.PointOfInterestStorage;
+import net.minecraft.world.poi.PointOfInterestTypes;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
+
+public class CustomTraderManager {
+
+    private final Random random;
+
+    public int cooldown;
+
+    //I'm so good at making bad decisions
+    public CustomTraderManager() {
+        this.random = Random.create();
+    }
+
+    public void tick() {
+        if (this.cooldown > 0) {
+            --this.cooldown;
+        }
+        //LogUtil.info(this.cooldown);
+    }
+
+    public void trySpawn(ServerWorld world, ServerWorldProperties properties, PlayerEntity player) {
+        if (cooldown == 0) if (player != null) {
+            BlockPos blockPos = player.getBlockPos();
+
+            PointOfInterestStorage pointOfInterestStorage = world.getPointOfInterestStorage();
+            Optional<BlockPos> optional = pointOfInterestStorage.getPosition(
+                    registryEntry -> registryEntry.matchesKey(PointOfInterestTypes.MEETING), pos -> true, blockPos, 48, PointOfInterestStorage.OccupationStatus.ANY
+            );
+            BlockPos blockPos2 = optional.orElse(blockPos);
+            BlockPos blockPos3 = getNearbySpawnPos(world, blockPos2, 48);
+            if (blockPos3 != null && doesNotSuffocateAt(world, blockPos3)) {
+                if (world.getBiome(blockPos3).isIn(BiomeTags.WITHOUT_WANDERING_TRADER_SPAWNS)) {
+                    return;
+                }
+
+                WanderingTraderEntity wanderingTraderEntity = EntityType.WANDERING_TRADER.spawn(world, null, null, null, blockPos3, SpawnReason.EVENT, false, false);
+                if (wanderingTraderEntity != null) {
+                    cooldown = 48000;
+                    for (int j = 0; j < 2; ++j) {
+                        spawnLlama(world, wanderingTraderEntity);
+                    }
+
+                    properties.setWanderingTraderId(wanderingTraderEntity.getUuid());
+                    wanderingTraderEntity.setDespawnDelay(48000);
+                    wanderingTraderEntity.setWanderTarget(blockPos2);
+                    wanderingTraderEntity.setPositionTarget(blockPos2, 16);
+                }
+            }
+        }
+    }
+
+    private void spawnLlama(ServerWorld world, WanderingTraderEntity wanderingTrader) {
+        BlockPos blockPos = this.getNearbySpawnPos(world, wanderingTrader.getBlockPos(), 4);
+        if (blockPos != null) {
+            TraderLlamaEntity traderLlamaEntity = EntityType.TRADER_LLAMA.spawn(world, null, null, null, blockPos, SpawnReason.EVENT, false, false);
+            if (traderLlamaEntity != null) {
+                traderLlamaEntity.attachLeash(wanderingTrader, true);
+            }
+        }
+    }
+
+    @Nullable
+    private BlockPos getNearbySpawnPos(WorldView world, BlockPos pos, int range) {
+        BlockPos blockPos = null;
+
+        for (int i = 0; i < 10; ++i) {
+            int j = pos.getX() + this.random.nextInt(range * 2) - range;
+            int k = pos.getZ() + this.random.nextInt(range * 2) - range;
+            int l = world.getTopY(Heightmap.Type.WORLD_SURFACE, j, k);
+            BlockPos blockPos2 = new BlockPos(j, l, k);
+            if (SpawnHelper.canSpawn(SpawnRestriction.Location.ON_GROUND, world, blockPos2, EntityType.WANDERING_TRADER)) {
+                blockPos = blockPos2;
+                break;
+            }
+        }
+
+        return blockPos;
+    }
+
+    private boolean doesNotSuffocateAt(BlockView world, BlockPos pos) {
+        for (BlockPos blockPos : BlockPos.iterate(pos, pos.add(1, 2, 1))) {
+            if (!world.getBlockState(blockPos).getCollisionShape(world, blockPos).isEmpty()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}

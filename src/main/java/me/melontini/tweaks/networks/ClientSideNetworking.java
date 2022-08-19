3a0cf28d3aa1f5b72b602c29aae410317c82eed0
melontini <melontini@yandex.ru>
2022-08-19 23:32:49 +0700

    I decided that there's no better way to waste my time than creating multi-version text abstractions only using reflection.

--- src/main/java/me/melontini/tweaks/networks/ClientSideNetworking.java
+++ src/main/java/me/melontini/tweaks/networks/ClientSideNetworking.java
@@ -3,14 +3,13 @@ package me.melontini.tweaks.networks;
 import me.melontini.tweaks.Tweaks;
 import me.melontini.tweaks.client.sound.PersistentMovingSoundInstance;
 import me.melontini.tweaks.util.LogUtil;
+import me.melontini.tweaks.util.TextUtil;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.MusicDiscItem;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.random.Random;
 
@@ -34,14 +33,14 @@ public class ClientSideNetworking {
                     Entity entity = client.world.getEntityLookup().get(id);
                     if (stack.getItem() instanceof MusicDiscItem disc) {
 
-                        MutableText discName = disc.getDescription();
+                        var discName = disc.getDescription();
                         SoundInstance instance = new PersistentMovingSoundInstance(disc.getSound(), SoundCategory.RECORDS, id, client.world, Random.create());
                         soundInstanceMap.put(id, instance);
                         client.getSoundManager().play(instance);
 
                         if (discName != null)
                             if (client.player != null) if (entity != null) if (entity.distanceTo(client.player) < 76) {
-                                client.player.sendMessage(Text.translatable("record.nowPlaying", discName), true);
+                                client.player.sendMessage(TextUtil.createTranslatable("record.nowPlaying", discName), true);
                             }
                     }
                 });

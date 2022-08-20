04c29d071564ea5e8067aad5198d5de73fd85760
melontini <melontini@yandex.ru>
2022-08-20 21:23:03 +0700

    oops

--- src/main/java/me/melontini/tweaks/mixin/items/clock_tooltip/ItemMixin.java
+++ src/main/java/me/melontini/tweaks/mixin/items/clock_tooltip/ItemMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -25,7 +26,7 @@ public class ItemMixin {
                 //totally not stolen from here https://bukkit.org/threads/how-can-i-convert-minecraft-long-time-to-real-hours-and-minutes.122912/
                 int i = (int) Math.floor((world.getTimeOfDay() / 1000 + 8) % 24);
                 int j = (int) Math.floor(60 * (world.getTimeOfDay() % 1000) / 1000);
-                tooltip.add(TextUtil.createTranslatable("tooltip.m-tweaks.clock", i, j));
+                tooltip.add(TextUtil.applyFormatting(TextUtil.createTranslatable("tooltip.m-tweaks.clock", String.format("%02d:%02d", i, j)), Formatting.GRAY));
             }
         }
     }

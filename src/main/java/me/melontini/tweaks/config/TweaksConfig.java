36ac88a01a1eb048d0522e19a28dfb2893b69740
melontini <melontini@yandex.ru>
2022-08-20 21:13:44 +0700

    clock tooltip & actually rainbow record text

--- src/main/java/me/melontini/tweaks/config/TweaksConfig.java
+++ src/main/java/me/melontini/tweaks/config/TweaksConfig.java
@@ -180,6 +180,11 @@ public class TweaksConfig implements ConfigData {
     }
 
     @ConfigEntry.Category("items")
+    @Comment("Shows in-game-time inside clock's tooltip")
+    @ConfigEntry.Gui.Tooltip
+    public boolean clockTooltip = true;
+
+    @ConfigEntry.Category("items")
     @Comment(" . ")
     @ConfigEntry.Gui.Tooltip(count = 4)
     public boolean balancedMending = false;

5bc05f8881b6e5d0ff83a7085c36216b5ca4c296
melontini <melontini@yandex.ru>
2022-08-28 13:03:59 +0700

    completely expected

--- src/main/java/me/melontini/tweaks/Tweaks.java
+++ src/main/java/me/melontini/tweaks/Tweaks.java
@@ -1,7 +1,5 @@
 package me.melontini.tweaks;
 
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Table;
 import me.melontini.tweaks.config.TweaksConfig;
 import me.melontini.tweaks.networks.ServerSideNetworking;
 import me.melontini.tweaks.registries.BlockRegistry;
@@ -19,10 +17,6 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
-import net.minecraft.entity.vehicle.BoatEntity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.Items;
 import net.minecraft.particle.DefaultParticleType;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.server.world.ServerWorld;
@@ -43,7 +37,6 @@ public class Tweaks implements ModInitializer {
     public static Map<Identifier, PlantData> PLANT_DATA = new HashMap<>();
     public static Map<Identifier, EggProcessingData> EGG_DATA = new HashMap<>();
     public static DefaultParticleType KNOCKOFF_TOTEM_PARTICLE = FabricParticleTypes.simple();
-    public static Table<Item, ItemGroup, Item> ITEM_GROUP_OVERRIDES = HashBasedTable.create();
 
     @Override
     public void onInitialize() {
@@ -52,30 +45,6 @@ public class Tweaks implements ModInitializer {
         EntityTypeRegistry.register();
         ServerSideNetworking.register();
         ResourceConditionRegistry.register();
-
-        for (BoatEntity.Type type : BoatEntity.Type.values()) {
-            if (Tweaks.CONFIG.newBoats.isFurnaceBoatOn)
-                ITEM_GROUP_OVERRIDES.put(Registry.ITEM.get(new Identifier(MODID, type.getName() + "_boat_with_furnace")), ItemGroup.TRANSPORTATION, Registry.ITEM.get(new Identifier("minecraft", type.getName() + "_chest_boat")));
-            if (Tweaks.CONFIG.newBoats.isJukeboxBoatOn)
-                ITEM_GROUP_OVERRIDES.put(Registry.ITEM.get(new Identifier(MODID, type.getName() + "_boat_with_jukebox")), ItemGroup.TRANSPORTATION, Registry.ITEM.get(new Identifier("minecraft", type.getName() + "_chest_boat")));
-            if (Tweaks.CONFIG.newBoats.isTNTBoatOn)
-                ITEM_GROUP_OVERRIDES.put(Registry.ITEM.get(new Identifier(MODID, type.getName() + "_boat_with_tnt")), ItemGroup.TRANSPORTATION, Registry.ITEM.get(new Identifier("minecraft", type.getName() + "_chest_boat")));
-            if (Tweaks.CONFIG.newBoats.isHopperBoatOn)
-                ITEM_GROUP_OVERRIDES.put(Registry.ITEM.get(new Identifier(MODID, type.getName() + "_boat_with_hopper")), ItemGroup.TRANSPORTATION, Registry.ITEM.get(new Identifier("minecraft", type.getName() + "_chest_boat")));
-        }
-
-        ITEM_GROUP_OVERRIDES.put(ItemRegistry.SPAWNER_MINECART, ItemGroup.TRANSPORTATION, Items.HOPPER_MINECART);
-        if (CONFIG.newMinecarts.isAnvilMinecartOn)
-            ITEM_GROUP_OVERRIDES.put(ItemRegistry.ANVIL_MINECART, ItemGroup.TRANSPORTATION, Items.HOPPER_MINECART);
-        if (CONFIG.newMinecarts.isNoteBlockMinecartOn)
-            ITEM_GROUP_OVERRIDES.put(ItemRegistry.NOTE_BLOCK_MINECART, ItemGroup.TRANSPORTATION, Items.HOPPER_MINECART);
-        if (CONFIG.newMinecarts.isJukeboxMinecartOn)
-            ITEM_GROUP_OVERRIDES.put(ItemRegistry.JUKEBOX_MINECART, ItemGroup.TRANSPORTATION, Items.HOPPER_MINECART);
-
-        if (CONFIG.incubatorSettings.enableIncubator)
-            ITEM_GROUP_OVERRIDES.put(ItemRegistry.INCUBATOR, ItemGroup.DECORATIONS, Items.STONECUTTER);
-        if (CONFIG.totemSettings.enableInfiniteTotem)
-            ITEM_GROUP_OVERRIDES.put(ItemRegistry.INFINITE_TOTEM, ItemGroup.COMBAT, Items.TOTEM_OF_UNDYING);
 
         if (CONFIG.usefulFletching) {
             FLETCHING_SCREEN_HANDLER = new ScreenHandlerType<>(FletchingScreenHandler::new);
--- src/main/java/me/melontini/tweaks/ducks/ItemGroupAccess.java
+++
@@ -1,5 +0,0 @@
-package me.melontini.tweaks.ducks;
-
-public interface ItemGroupAccess {
-    void mTweaks$initItems();
-}

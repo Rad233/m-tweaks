af17ad7085e387132e530537dc743d6e79aa5a1d
melontini <melontini@yandex.ru>
2022-08-28 18:41:19 +0700

    meh

--- src/main/java/me/melontini/tweaks/mixin/items/infinite_totem/ItemEntityMixin.java
+++ src/main/java/me/melontini/tweaks/mixin/items/infinite_totem/ItemEntityMixin.java
@@ -17,6 +17,7 @@ import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Pair;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.world.Heightmap;
@@ -39,61 +40,66 @@ public abstract class ItemEntityMixin extends Entity implements ItemEntityFriend
     @Shadow
     @Final
     private static TrackedData<ItemStack> STACK;
-
-    @Shadow public abstract void setPickupDelayInfinite();
-
+    private final Random mTweaks$random = new Random();
     private int mTweaks$ascensionTicks;
-
-    private Optional<ItemEntity> itemEntity = Optional.empty();
-
+    private Optional<ItemEntity> mTweaks$itemEntity = Optional.empty();
+    private Pair<BeaconBlockEntity, Integer> mTweaks$beacon = new Pair<>(null, 0);
 
     public ItemEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
+
+    @Shadow
+    public abstract void setPickupDelayInfinite();
+
+    @Shadow
+    public abstract void setToDefaultPickupDelay();
 
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;tick()V", shift = At.Shift.AFTER), method = "tick")
     private void mTweaks$tick(CallbackInfo ci) {
         if (Tweaks.CONFIG.totemSettings.enableTotemAscension && Tweaks.CONFIG.totemSettings.enableInfiniteTotem) {
             ItemEntity self = (ItemEntity) (Object) this;
             if (this.dataTracker.get(STACK).isOf(Items.TOTEM_OF_UNDYING)) {
-                BlockEntity entity = world.getBlockEntity(new BlockPos(getX(), world.getTopY(Heightmap.Type.WORLD_SURFACE, getBlockPos().getX(), getBlockPos().getZ()) - 1, getZ()));
-                if (entity instanceof BeaconBlockEntity blockEntity && BeaconUtil.getLevelFromBlocks(world, blockEntity.getPos(), List.of(Blocks.DIAMOND_BLOCK, Blocks.NETHERITE_BLOCK)) == 4) {
-                    if (itemEntity.isEmpty()) {
-                        if (mTweaks$ascensionTicks > 0) --mTweaks$ascensionTicks;
-                        if (world.getTime() % 20 == 0) {
-                            List<ItemEntity> list = world.getEntitiesByClass(ItemEntity.class, new Box(getPos().x + 0.5, getPos().y + 0.5, getPos().z + 0.5, getPos().x - 0.5, getPos().y - 0.5, getPos().z - 0.5), itemEntity1 -> itemEntity1.getDataTracker().get(STACK).isOf(Items.NETHER_STAR)).stream().filter(itemEntity -> ((ItemEntityFriendAccess) itemEntity).mTweaks$getFriend() == self || !((ItemEntityFriendAccess) itemEntity).mTweaks$hasFriend()).toList();
-                            if (!list.isEmpty()) {
-                                itemEntity = list.stream().min(Comparator.comparingDouble(itemEntity2 -> itemEntity2.squaredDistanceTo(self)));
+                if (age % 35 == 0 && mTweaks$ascensionTicks == 0) mTweaks$beaconCheck();
 
-                                int i = itemEntity.get().getDataTracker().get(STACK).getCount() - 1;
+                if (mTweaks$beacon.getLeft() != null && mTweaks$beacon.getRight() == 4) {
+                    if (mTweaks$itemEntity.isEmpty()) {
+                        if (mTweaks$ascensionTicks > 0) --mTweaks$ascensionTicks;
+                        if (age % 35 == 0) {
+                            List<ItemEntity> list = world.getEntitiesByClass(ItemEntity.class, new Box(getPos().x + 0.5, getPos().y + 0.5, getPos().z + 0.5, getPos().x - 0.5, getPos().y - 0.5, getPos().z - 0.5),
+                                    itemEntity1 -> itemEntity1.getDataTracker().get(STACK).isOf(Items.NETHER_STAR) && (((ItemEntityFriendAccess) itemEntity1).mTweaks$getFriend() == self || !((ItemEntityFriendAccess) itemEntity1).mTweaks$hasFriend()));
+
+                            if (!list.isEmpty()) {
+                                mTweaks$itemEntity = list.stream().min(Comparator.comparingDouble(itemEntity2 -> itemEntity2.squaredDistanceTo(self)));
+
+                                int i = mTweaks$itemEntity.get().getDataTracker().get(STACK).getCount() - 1;
                                 if (i != 0) {
-                                    ItemStack entityStack = itemEntity.get().getDataTracker().get(STACK).copy();
+                                    ItemStack entityStack = mTweaks$itemEntity.get().getDataTracker().get(STACK).copy();
                                     entityStack.setCount(i);
-                                    itemEntity.get().getDataTracker().get(STACK).setCount(1);
+                                    mTweaks$itemEntity.get().getDataTracker().get(STACK).setCount(1);
                                     ItemEntity itemEntity1 = EntityType.ITEM.create(world);
                                     itemEntity1.setStack(entityStack);
-                                    itemEntity1.setPos(itemEntity.get().getX(), itemEntity.get().getY() + 0.3, itemEntity.get().getZ());
+                                    itemEntity1.setPos(mTweaks$itemEntity.get().getX(), mTweaks$itemEntity.get().getY() + 0.3, mTweaks$itemEntity.get().getZ());
                                     world.spawnEntity(itemEntity1);
                                 }
-                                itemEntity.get().setPickupDelayInfinite();
+                                mTweaks$itemEntity.get().setPickupDelayInfinite();
                                 this.setPickupDelayInfinite();
-                                ((ItemEntityFriendAccess) itemEntity.get()).mTweaks$setFriend(self);
+                                ((ItemEntityFriendAccess) mTweaks$itemEntity.get()).mTweaks$setFriend(self);
                             }
                         }
                     } else {
-                        if (((ItemEntityFriendAccess) itemEntity.get()).mTweaks$getFriend() != self) {
-                            itemEntity = Optional.empty();
+                        if (((ItemEntityFriendAccess) mTweaks$itemEntity.get()).mTweaks$getFriend() != self) {
+                            mTweaks$itemEntity = Optional.empty();
                         } else {
-                            Random random1 = new Random();
-                            if (mTweaks$ascensionTicks < 180) {
+                            if (mTweaks$ascensionTicks < 180 && mTweaks$beaconCheck()) {
                                 ++mTweaks$ascensionTicks;
-                                itemEntity.get().setVelocity(itemEntity.get().getVelocity().x * .5, .07, itemEntity.get().getVelocity().z * .5);
+                                mTweaks$itemEntity.get().setVelocity(mTweaks$itemEntity.get().getVelocity().x * .5, .07, mTweaks$itemEntity.get().getVelocity().z * .5);
                                 self.setVelocity(getVelocity().x * .5, .07, getVelocity().z * .5);
                                 if (world.isClient) {
-                                    if (random1.nextInt(8) == 0)
+                                    if (mTweaks$random.nextInt(8) == 0)
                                         world.addParticle(ParticleTypes.END_ROD, self.getX(), self.getY(), self.getZ(), getVelocity().x, -.07, getVelocity().z);
-                                    if (random1.nextInt(8) == 0)
-                                        world.addParticle(ParticleTypes.END_ROD, itemEntity.get().getX(), itemEntity.get().getY(), itemEntity.get().getZ(), itemEntity.get().getVelocity().x, -.07, itemEntity.get().getVelocity().z);
+                                    if (mTweaks$random.nextInt(8) == 0)
+                                        world.addParticle(ParticleTypes.END_ROD, mTweaks$itemEntity.get().getX(), mTweaks$itemEntity.get().getY(), mTweaks$itemEntity.get().getZ(), mTweaks$itemEntity.get().getVelocity().x, -.07, mTweaks$itemEntity.get().getVelocity().z);
                                 }
                             } else if (mTweaks$ascensionTicks == 180) {
                                 mTweaks$ascensionTicks = 0;
@@ -101,15 +107,31 @@ public abstract class ItemEntityMixin extends Entity implements ItemEntityFriend
                                 ItemEntity itemEntity2 = new ItemEntity(world, self.getX(), self.getY(), self.getZ(), stack);
                                 world.spawnEntity(itemEntity2);
                                 if (!world.isClient()) {
-                                    ((ServerWorld) world).spawnParticles(Tweaks.KNOCKOFF_TOTEM_PARTICLE, itemEntity2.getX(), itemEntity2.getY(), itemEntity2.getZ(), 19, random1.nextDouble(0.4) - 0.2, random1.nextDouble(0.4) - 0.2, random1.nextDouble(0.4) - 0.2, 0.5);
+                                    ((ServerWorld) world).spawnParticles(Tweaks.KNOCKOFF_TOTEM_PARTICLE, itemEntity2.getX(), itemEntity2.getY(), itemEntity2.getZ(), 19, mTweaks$random.nextDouble(0.4) - 0.2, mTweaks$random.nextDouble(0.4) - 0.2, mTweaks$random.nextDouble(0.4) - 0.2, 0.5);
                                 }
-                                itemEntity.get().discard();
+                                mTweaks$itemEntity.get().discard();
                                 self.discard();
+                            } else if (!mTweaks$beaconCheck()) {
+                                mTweaks$ascensionTicks = 0;
+                                setToDefaultPickupDelay();
+                                mTweaks$itemEntity.get().setToDefaultPickupDelay();
+                                ((ItemEntityFriendAccess) mTweaks$itemEntity.get()).mTweaks$setFriend(null);
                             }
                         }
                     }
                 }
             }
+        }
+    }
+
+    private boolean mTweaks$beaconCheck() {
+        BlockEntity entity = world.getBlockEntity(new BlockPos(getX(), world.getTopY(Heightmap.Type.WORLD_SURFACE, getBlockPos().getX(), getBlockPos().getZ()) - 1, getZ()));
+        if (entity instanceof BeaconBlockEntity beaconBlock) {
+            this.mTweaks$beacon = new Pair<>(beaconBlock, BeaconUtil.getLevelFromBlocks(world, beaconBlock.getPos(), List.of(Blocks.DIAMOND_BLOCK, Blocks.NETHERITE_BLOCK)));
+            return true;
+        } else {
+            this.mTweaks$beacon = new Pair<>(null, 0);
+            return false;
         }
     }
 

49d373a6ceeb8d53b0e0969a4e145d10de409115
melontini <104443436+melontini@users.noreply.github.com>
2022-11-28 22:43:32 +0700

    stop

--- src/main/java/me/melontini/tweaks/util/DrawUtil.java
+++ src/main/java/me/melontini/tweaks/util/DrawUtil.java
@@ -169,4 +169,35 @@ public class DrawUtil {
         builder.vertex(matrix, startX, endY, z).color(k, l, m, j).next();
         builder.vertex(matrix, endX, endY, z).color(k, l, m, j).next();
     }
+
+    public static void drawTexture(MatrixStack matrices, float x, float y, float z, float u, float v, float width, float height) {
+        drawTexture(matrices, x, y, z, u, v, width, height, 256, 256);
+    }
+
+    public static void drawTexture(MatrixStack matrices, float x, float y, float z, float u, float v, float width, float height, float textureWidth, float textureHeight) {
+        drawTexture(matrices, x, x + width, y, y + height, z, width, height, u, v, textureWidth, textureHeight);
+    }
+
+    public static void drawTexture(MatrixStack matrices, float x, float y, float width, float height, float u, float v, float regionWidth, float regionHeight, float textureWidth, float textureHeight) {
+        drawTexture(matrices, x, x + width, y, y + height, 0, regionWidth, regionHeight, u, v, textureWidth, textureHeight);
+    }
+
+    public static void drawTexture(MatrixStack matrices, float x, float y, float u, float v, float width, float height, float textureWidth, float textureHeight) {
+        drawTexture(matrices, x, y, width, height, u, v, width, height, textureWidth, textureHeight);
+    }
+
+    private static void drawTexture(MatrixStack matrices, float x0, float x1, float y0, float y1, float z, float regionWidth, float regionHeight, float u, float v, float textureWidth, float textureHeight) {
+        drawTexturedQuad(matrices.peek().getPositionMatrix(), x0, x1, y0, y1, z, (u + 0.0F) / textureWidth, (u + regionWidth) / textureWidth, (v + 0.0F) / textureHeight, (v + regionHeight) / textureHeight);
+    }
+
+    private static void drawTexturedQuad(Matrix4f matrix, float x0, float x1, float y0, float y1, float z, float u0, float u1, float v0, float v1) {
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
+        bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
+        bufferBuilder.vertex(matrix, x0, y1, z).texture(u0, v1).next();
+        bufferBuilder.vertex(matrix, x1, y1, z).texture(u1, v1).next();
+        bufferBuilder.vertex(matrix, x1, y0, z).texture(u1, v0).next();
+        bufferBuilder.vertex(matrix, x0, y0, z).texture(u0, v0).next();
+        BufferRenderer.drawWithShader(bufferBuilder.end());
+    }
 }

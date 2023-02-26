1741bdbec2e2fea5b65ed062288b6b727c37f384
melontini <104443436+melontini@users.noreply.github.com>
2023-02-26 17:02:18 +0700

    oh well 2

--- src/main/java/me/melontini/tweaks/registries/BlockRegistry.java
+++ src/main/java/me/melontini/tweaks/registries/BlockRegistry.java
@@ -1,10 +1,12 @@
 package me.melontini.tweaks.registries;
 
-import me.melontini.crackerutil.content.RegistryUtil;
+import me.melontini.crackerutil.content.ContentBuilder;
 import me.melontini.tweaks.Tweaks;
 import me.melontini.tweaks.blocks.IncubatorBlock;
 import me.melontini.tweaks.blocks.entities.IncubatorBlockEntity;
+import me.melontini.tweaks.items.RoseOfTheValley;
 import me.melontini.tweaks.util.LogUtil;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Blocks;
@@ -12,18 +14,32 @@ import net.minecraft.block.FlowerBlock;
 import net.minecraft.block.Material;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 
 import static me.melontini.tweaks.Tweaks.MODID;
 
 public class BlockRegistry {
-    public static FlowerBlock ROSE_OF_THE_VALLEY = (FlowerBlock) RegistryUtil.createBlock(Tweaks.CONFIG.unknown, FlowerBlock.class, new Identifier(MODID, "rose_of_the_valley"), StatusEffects.REGENERATION, 12, AbstractBlock.Settings.copy(Blocks.LILY_OF_THE_VALLEY));
-    public static IncubatorBlock INCUBATOR_BLOCK = (IncubatorBlock) RegistryUtil.createBlock(Tweaks.CONFIG.incubatorSettings.enableIncubator, IncubatorBlock.class, new Identifier(MODID, "incubator"), FabricBlockSettings.of(Material.WOOD).strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD));
+    public static FlowerBlock ROSE_OF_THE_VALLEY = ContentBuilder.BlockBuilder
+            .create(FlowerBlock.class, new Identifier(MODID, "rose_of_the_valley"), StatusEffects.REGENERATION, 12, AbstractBlock.Settings.copy(Blocks.LILY_OF_THE_VALLEY))
+            .loadCondition(Tweaks.CONFIG.unknown)
+            .itemBuilder((block, id) -> ContentBuilder.ItemBuilder
+                    .create(RoseOfTheValley.class, id, block, new FabricItemSettings().rarity(Rarity.UNCOMMON))).build();
+    public static IncubatorBlock INCUBATOR_BLOCK = ContentBuilder.BlockBuilder
+            .create(IncubatorBlock.class, new Identifier(MODID, "incubator"), FabricBlockSettings.of(Material.WOOD).strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD))
+            .loadCondition(Tweaks.CONFIG.incubatorSettings.enableIncubator)
+            .itemBuilder((block, id) -> ContentBuilder.ItemBuilder
+                    .create(BlockItem.class, id, block, new FabricItemSettings().rarity(Rarity.RARE))
+                    .itemGroup(ItemGroups.REDSTONE)).build();
+
+    public static BlockEntityType<IncubatorBlockEntity> INCUBATOR_BLOCK_ENTITY = ContentBuilder.BlockEntityBuilder
+            .create(new Identifier(MODID, "incubator"), IncubatorBlockEntity::new, INCUBATOR_BLOCK)
+            .loadCondition(INCUBATOR_BLOCK != null).build();
 
     public static void register() {
-        LogUtil.devInfo("BlockRegistry init complete!");
-    }    public static BlockEntityType<IncubatorBlockEntity> INCUBATOR_BLOCK_ENTITY = RegistryUtil.createBlockEntity(INCUBATOR_BLOCK != null, new Identifier(MODID, "incubator"), BlockEntityType.Builder.create(IncubatorBlockEntity::new, INCUBATOR_BLOCK));
-
-
+        LogUtil.info("BlockRegistry init complete!");
+    }
 }

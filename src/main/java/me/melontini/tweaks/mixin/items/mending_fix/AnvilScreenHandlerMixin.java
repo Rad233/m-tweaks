cace299a6f8d4eeafebd702f357b728b30ce99dc
melontini <104443436+melontini@users.noreply.github.com>
2023-04-08 20:23:22 +0700

    I love MixinExtras

--- src/main/java/me/melontini/tweaks/mixin/items/mending_fix/AnvilScreenHandlerMixin.java
+++ src/main/java/me/melontini/tweaks/mixin/items/mending_fix/AnvilScreenHandlerMixin.java
@@ -1,7 +1,11 @@
 package me.melontini.tweaks.mixin.items.mending_fix;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import me.melontini.tweaks.Tweaks;
 import me.melontini.tweaks.util.annotations.MixinRelatedConfigOption;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerInventory;
@@ -25,24 +29,18 @@ import java.util.Map;
 @MixinRelatedConfigOption("balancedMending")
 @Mixin(AnvilScreenHandler.class)
 public abstract class AnvilScreenHandlerMixin extends ForgingScreenHandler {
-    @Unique
-    private ItemStack mTweaks$fakeStack = ItemStack.EMPTY;
 
     public AnvilScreenHandlerMixin(@org.jetbrains.annotations.Nullable ScreenHandlerType<?> type, int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
         super(type, syncId, playerInventory, context);
     }
 
-    @ModifyArg(method = "updateResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/inventory/CraftingResultInventory;setStack(ILnet/minecraft/item/ItemStack;)V", ordinal = 4), index = 1)
-    private ItemStack mTweaks$setFakeStack(ItemStack stack) {
-        if (Tweaks.CONFIG.balancedMending) if (!this.getSlot(1).getStack().isOf(Items.ENCHANTED_BOOK))
-            if (EnchantmentHelper.get(mTweaks$fakeStack).containsKey(Enchantments.MENDING)) {
-                return mTweaks$fakeStack;
-            }
-        return stack;
-    }
-
-    @Inject(at = @At(value = "FIELD", target = "net/minecraft/item/ItemStack.EMPTY : Lnet/minecraft/item/ItemStack;", opcode = Opcodes.GETSTATIC, ordinal = 5, shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILSOFT, method = "updateResult")
-    private void mTweaks$updateResult(CallbackInfo ci, ItemStack itemStack, int i, int j, int k, ItemStack itemStack2, ItemStack itemStack3, Map map) {
-        if (Tweaks.CONFIG.balancedMending) mTweaks$fakeStack = itemStack2.copy();
+    @ModifyExpressionValue(method = "updateResult", at = @At(value = "CONSTANT", args = "intValue=40"))
+    private int mTweaks$setRepairLimit(int constant) {
+        if (Tweaks.CONFIG.balancedMending)
+            if (!this.getSlot(1).getStack().isOf(Items.ENCHANTED_BOOK))
+                if (EnchantmentHelper.get(this.getSlot(0).getStack()).containsKey(Enchantments.MENDING)) {
+                    return Integer.MAX_VALUE;
+                }
+        return constant;
     }
 }

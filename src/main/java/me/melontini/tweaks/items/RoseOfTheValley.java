f339055169f68363a2746dd719b5badf6c4bc0df
melontini <104443436+melontini@users.noreply.github.com>
2023-04-12 20:43:56 +0700

    try cracker's new particles

--- src/main/java/me/melontini/tweaks/items/RoseOfTheValley.java
+++ src/main/java/me/melontini/tweaks/items/RoseOfTheValley.java
@@ -1,6 +1,5 @@
 package me.melontini.tweaks.items;
 
-import me.melontini.crackerutil.client.particles.Particle;
 import me.melontini.crackerutil.client.util.ScreenParticleHelper;
 import me.melontini.tweaks.registries.ItemRegistry;
 import me.melontini.tweaks.util.TweaksTexts;
@@ -8,17 +7,13 @@ import net.minecraft.block.Block;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.StackReference;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.screen.slot.Slot;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.text.Text;
-import net.minecraft.util.ClickType;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
-import java.awt.*;
 import java.util.List;
 
 public class RoseOfTheValley extends BlockItem {
@@ -40,15 +35,7 @@ public class RoseOfTheValley extends BlockItem {
             var client = MinecraftClient.getInstance();
             int x = (int) (client.mouse.getX() * (double) client.getWindow().getScaledWidth() / (double) client.getWindow().getWidth());
             int y = (int) (client.mouse.getY() * (double) client.getWindow().getScaledHeight() / (double) client.getWindow().getHeight());
-            for (int i = 0; i < 20; i++) {
-                ScreenParticleHelper.addParticle(new Particle(x, y, MathHelper.nextDouble(player.world.random, -1, 1), MathHelper.nextDouble(player.world.random, -1, -0.5), Color.RED.darker().getRGB()) {
-                    @Override
-                    protected void tickLogic() {
-                        x += Math.sin((player.age) / 8.0 * velX);
-                        y += velY * 0.99;
-                    }
-                });
-            }
+            ScreenParticleHelper.addParticles(ParticleTypes.END_ROD, x, y, 0.5, 0.5, 0.08, 10);
         }
     }
 }

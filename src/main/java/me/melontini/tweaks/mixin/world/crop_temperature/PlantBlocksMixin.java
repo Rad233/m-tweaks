cfda759b709ea834dbc3ce6073d11d899a3cc2ed
melontini <104443436+melontini@users.noreply.github.com>
2023-04-15 00:27:42 +0700

    "version independent"

--- src/main/java/me/melontini/tweaks/mixin/world/crop_temperature/PlantBlocksMixin.java
+++ src/main/java/me/melontini/tweaks/mixin/world/crop_temperature/PlantBlocksMixin.java
@@ -1,13 +1,13 @@
 package me.melontini.tweaks.mixin.world.crop_temperature;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import me.melontini.crackerutil.util.Utilities;
 import me.melontini.tweaks.Tweaks;
-import me.melontini.tweaks.util.LogUtil;
 import me.melontini.tweaks.util.annotations.MixinRelatedConfigOption;
 import me.melontini.tweaks.util.data.PlantData;
 import net.minecraft.block.*;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,14 +18,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Pseudo
 @Mixin(value = {AbstractPlantStemBlock.class, BambooBlock.class, CactusBlock.class, CocoaBlock.class, CropBlock.class, SaplingBlock.class, SugarCaneBlock.class, SweetBerryBushBlock.class})
 public class PlantBlocksMixin {
-    @Inject(at = @At("HEAD"), method = "randomTick", cancellable = true)
-    private void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+    @Inject(at = @At("HEAD"), method = "randomTick", cancellable = true) //why? Because Mojang random
+    private void randomTick(CallbackInfo ci, @Local(argsOnly = true, index = 1) BlockState state, @Local(argsOnly = true, index = 2) ServerWorld world, @Local(argsOnly = true, index = 3) BlockPos pos) {
         if (Tweaks.CONFIG.temperatureBasedCropGrowthSpeed) {
             PlantData data = Tweaks.PLANT_DATA.get((Block) (Object) this);
             if (data != null) {
                 float temp = world.getBiome(pos).value().getTemperature();
                 if ((temp > data.max && temp <= data.aMax) || (temp < data.min && temp >= data.aMin)) {
-                    if (random.nextInt(2) == 0) {
+                    if (Utilities.RANDOM.nextInt(2) == 0) {
                         ci.cancel();
                     }
                 } else if ((temp > data.aMax) || (temp < data.aMin)) {

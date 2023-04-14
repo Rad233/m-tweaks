dde59737e81d5c42162aa407ea82a30c75de8931
melontini <104443436+melontini@users.noreply.github.com>
2023-04-15 01:57:53 +0700

    sorry not sorry

--- src/main/java/me/melontini/tweaks/mixin/entities/flower_duplication/BoneMealItemMixin.java
+++ src/main/java/me/melontini/tweaks/mixin/entities/flower_duplication/BoneMealItemMixin.java
@@ -4,10 +4,13 @@ import me.melontini.tweaks.Tweaks;
 import me.melontini.tweaks.util.annotations.MixinRelatedConfigOption;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.TallFlowerBlock;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.BoneMealItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,6 +23,13 @@ public class BoneMealItemMixin {
     private static void mTweaks$useOnFertilizable(ItemStack stack, World world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
         BlockState blockState = world.getBlockState(pos);
         if (blockState.getBlock() instanceof TallFlowerBlock && Tweaks.CONFIG.beeTallFlowerDuplication) {
+            if (!world.isClient) {
+                if (Tweaks.CONFIG.unknown && world.random.nextInt(100) == 0) {
+                    world.createExplosion(null, DamageSource.explosion((LivingEntity) null), null,
+                            pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 3.0F,
+                            false, Explosion.DestructionType.DESTROY);
+                }
+            }
             cir.setReturnValue(false);
         }
     }

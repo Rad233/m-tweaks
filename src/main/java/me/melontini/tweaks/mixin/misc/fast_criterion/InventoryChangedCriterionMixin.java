80f26f963ec985976e45811ce454618fe3769792
melontini <104443436+melontini@users.noreply.github.com>
2023-04-15 17:15:00 +0700

    impressive 2

--- src/main/java/me/melontini/tweaks/mixin/misc/fast_criterion/InventoryChangedCriterionMixin.java
+++ src/main/java/me/melontini/tweaks/mixin/misc/fast_criterion/InventoryChangedCriterionMixin.java
@@ -1,8 +1,9 @@
-package me.melontini.tweaks.mixin.bugfixes;
+package me.melontini.tweaks.mixin.misc.fast_criterion;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import me.melontini.tweaks.Tweaks;
+import me.melontini.tweaks.util.annotations.MixinRelatedConfigOption;
 import net.minecraft.advancement.criterion.InventoryChangedCriterion;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
@@ -12,6 +13,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(InventoryChangedCriterion.class)
+@MixinRelatedConfigOption("concurrentInvChangedTrigger")
 public class InventoryChangedCriterionMixin {
     @WrapOperation(method = "trigger(Lnet/minecraft/server/network/ServerPlayerEntity;Lnet/minecraft/entity/player/PlayerInventory;Lnet/minecraft/item/ItemStack;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancement/criterion/InventoryChangedCriterion;trigger(Lnet/minecraft/server/network/ServerPlayerEntity;Lnet/minecraft/entity/player/PlayerInventory;Lnet/minecraft/item/ItemStack;III)V"))
     private void mTweaks$triggerConcurrently(InventoryChangedCriterion criterion, ServerPlayerEntity player, PlayerInventory inventory, ItemStack stack, int full, int empty, int occupied, Operation<Void> operation) {

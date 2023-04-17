586b30c32404ee8112020372eb3301262e09b319
melontini <104443436+melontini@users.noreply.github.com>
2023-04-17 21:11:40 +0700

    Test #4. No configs yet.

---
+++ src/main/java/me/melontini/tweaks/mixin/items/extended_behavior/ItemStackMixin.java
@@ -0,0 +1,46 @@
+package me.melontini.tweaks.mixin.items.extended_behavior;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import me.melontini.tweaks.duck.ThrowableBehaviorDuck;
+import me.melontini.tweaks.entity.FlyingItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ItemStack.class)
+public abstract class ItemStackMixin {
+    @Shadow public abstract Item getItem();
+
+    @Shadow public abstract void decrement(int amount);
+
+    @ModifyReturnValue(at = @At("RETURN"), method = "use")
+    private TypedActionResult<ItemStack> mTweaks$throwableBehaviour(TypedActionResult<ItemStack> original, World world, PlayerEntity user, Hand hand) {
+        if (original.getResult() == ActionResult.PASS && ((ThrowableBehaviorDuck)getItem()).mTweaks$hasBehavior()) {
+            world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (world.random.nextFloat() * 0.4F + 0.8F));
+            if (!world.isClient) {
+                var entity = new FlyingItemEntity((ItemStack) (Object) this, user, world);
+                entity.setPos(user.getX(), user.getEyeY() - 0.1F, user.getZ());
+                entity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1.5F, 1.0F);
+                world.spawnEntity(entity);
+            }
+
+            user.incrementStat(Stats.USED.getOrCreateStat(getItem()));
+            if (!user.getAbilities().creativeMode) {
+                this.decrement(1);
+            }
+
+            return TypedActionResult.success((ItemStack) (Object) this);
+        }
+        return original;
+    }
+}
